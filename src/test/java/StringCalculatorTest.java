399f3f517a3e7777a5f2759cf05baaef5097fad6
Tien <Hoang>
2013-06-06 14:35:29 +0700

    Test 7: Red

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -53,11 +53,17 @@ public class StringCalculatorTest {
         stringCalculator.Add("//;\n,-1,2,-3");
     }
 
+
+
     @Test
     public void testWithInputNumberOverThanOneThousand () {
         StringCalculator stringCalculator = new StringCalculator();
         Assert.assertEquals(6,stringCalculator.Add("1,2,3;1001"));
     }
 
-
+    @Test
+    public void testWithDefineDelimiter() {
+        StringCalculator stringCalculator = new StringCalculator();
+        Assert.assertEquals(6, stringCalculator.Add("//[***]\n1***2***3"));
+    }
 }
